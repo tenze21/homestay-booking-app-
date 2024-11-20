1d043b5ffb042df8042f42c1efebce40d834e927
tenze21 <12230054.gcit@rub.edu.bt>
2024-11-20 16:20:15 +0600

    fix

--- server/app.js
+++ server/app.js
@@ -34,21 +34,21 @@ app.use("/api/user/upload", profileUpdateRoute);
 app.use("/api/homestay", homestayImageUpdateRoute);
 app.use("/api/reviews", reviewRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   const __dirname = path.resolve();
-//   app.use("/uploads", express.static("var/data/uploads"));
-//   app.use(express.static(path.join(__dirname, "/client/build")));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  // app.use("/uploads", express.static("var/data/uploads"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// } else {
-//   const __dirname = path.resolve();
-//   app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-//   app.get("/", (req, res) => {
-//     res.send("API is running...");
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
